477e99607f1ad9061d0eff7eafbb5e6c6bd18aa7
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-21 23:11:51 +0900

    MODEL-B-TEST-2: Extract (10~300) concepts

--- fake-news.R
+++ fake-news.R
@@ -36,11 +36,11 @@ tridf <- weightTfIdf(tdm)
 # Initialize an empty data frame to store the results
 results.df <- data.frame(dims=integer(), tn=integer(), fn=integer(), fp=integer(), tp=integer(), accuracy=numeric(), stringsAsFactors = FALSE)
 
-for (i in 10:100) {
+for (i in 10:300) {
 
   print(i)
 
-  # Extract (10~100) concepts
+  # Extract (10~300) concepts
   lsa.tfidf <- lsa(tridf, dims = i)
 
   # Convert to data frame
